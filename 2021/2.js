a98babb4d7b6151886ba0d9beac82a64acdba32b
Albert Zhang <alchzh@gmail.com>
2021-12-02 17:56:11 -0500

    Update 2.js

--- 2021/2.js
+++ 2021/2.js
@@ -1,3 +1,5 @@
-h = d = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; break} case 'down': {d += i; break}; case 'up': {d -= i; break}}; console.log(s, h, d)})
+h = d = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; break} case 'down': {d += i; break}; case 'up': {d -= i; break}}; /* console.log(s, h, d) */});
+console.log(`Part 1: ${h*d}`)
 
-h = d = a = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; d += a*i; break} case 'down': {a += i; break}; case 'up': {a -= i; break}}; console.log(s, h, d)})
+h = d = a = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; d += a*i; break} case 'down': {a += i; break}; case 'up': {a -= i; break}}; /* console.log(s, h, d) */})
+console.log(`Part 2: ${h*d}`)
