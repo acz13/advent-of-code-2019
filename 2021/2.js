f11ff68fe3f2392fc1eafec86193985b80ef7933
Albert Zhang <alchzh@gmail.com>
2021-12-02 17:55:07 -0500

    Update 2.js

--- 2021/2.js
+++ 2021/2.js
@@ -1,1 +1,3 @@
+h = d = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; break} case 'down': {d += i; break}; case 'up': {d -= i; break}}; console.log(s, h, d)})
+
 h = d = a = 0; document.body.textContent.trim().split("\n").forEach(s => {let [k, i] = s.split(" "); i = +i; switch (k) {case 'forward': {h += i; d += a*i; break} case 'down': {a += i; break}; case 'up': {a -= i; break}}; console.log(s, h, d)})
