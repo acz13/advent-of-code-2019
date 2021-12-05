5950d6c9d88dc869c6c5b5b169f1ffb37cc88baf
Albert Zhang <alchzh@gmail.com>
2021-12-05 00:31:21 -0500

    inputs and day05

---
+++ 2021/day05/day05.py
@@ -0,0 +1,29 @@
+with open("input.txt", "r") as f:
+    points = [[int(x) for k in line.strip().split(" -> ") for x in k.split(",")] for line in f.readlines()]
+
+fpoints = [l for l in points if l[0] == l[2] or l[1] == l[3]]
+dpoints = [l for l in points if l[0] != l[2] and l[1] != l[3]]
+
+covered = {}
+for l in fpoints:
+    for x in range(min(l[0], l[2]), max(l[0], l[2])+1):
+        for y in range(min(l[1], l[3]), max(l[1], l[3])+1):
+            if f"{x},{y}" in covered:
+                covered[f"{x},{y}"] += 1
+            else:
+                covered[f"{x},{y}"] = 1
+
+print("Part 1", sum(1 for i in covered.values() if i > 1))
+
+for l in dpoints:
+    d1 = int(l[2] > l[0]) or -1
+    d2 = int(l[3] > l[1]) or -1
+    for i in range(abs(l[2] - l[0]) + 1):
+        x = l[0] + d1 * i
+        y = l[1] + d2 * i
+        if f"{x},{y}" in covered:
+            covered[f"{x},{y}"] += 1
+        else:
+            covered[f"{x},{y}"] = 1
+
+print("Part 2", sum(1 for i in covered.values() if i > 1))
