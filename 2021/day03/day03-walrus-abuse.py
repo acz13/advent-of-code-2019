6c01cfea18bf51e1f65d7d4ea19182c4923adcf8
Albert Zhang <alchzh@gmail.com>
2021-12-12 21:21:48 -0500

    Use open(0) instead of __import__

--- 2021/day03/day03-walrus-abuse.py
+++ 2021/day03/day03-walrus-abuse.py
@@ -1,1 +1,1 @@
-print((f:=lambda d,j:int([(j:=[k for k in j if(k[b]==d)==(sum(int(i[b])for i in j)*2>=len(j))])for b in range(len(j[0]))if len(j)>1][-1][0],2))("1",(l:=__import__("sys").stdin.readlines()))*f("0",l))
+print((f:=lambda d,j:int([(j:=[k for k in j if(k[b]==d)==(sum(int(i[b])for i in j)*2>=len(j))])for b in range(len(j[0]))if len(j)>1][-1][0],2))("1",(l:=open(0).readlines()))*f("0",l))
