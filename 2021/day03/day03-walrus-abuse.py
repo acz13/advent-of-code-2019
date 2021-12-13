183a9ff10a214bf1f1132a803bc575f895d93346
Albert Zhang <alchzh@gmail.com>
2021-12-12 21:38:00 -0500

    Save two bytes with another walrus

--- 2021/day03/day03-walrus-abuse.py
+++ 2021/day03/day03-walrus-abuse.py
@@ -1,1 +1,1 @@
-print((f:=lambda d,j:int([(j:=[k for k in j if(k[b]!=d)^(sum(int(i[b])for i in j)*2>=len(j))])for b in range(len(j[0]))if len(j)>1][-1][0],2))("1",(l:=list(open(0))))*f("0",l))
+print((f:=lambda d,j:int([(j:=[k for k in j if(k[b]!=d)^(sum(int(i[b])for i in j)*2>c)])for b in range(len(j[0]))if(c:=len(j)-1)][-1][0],2))("1",(l:=list(open(0))))*f("0",l))
